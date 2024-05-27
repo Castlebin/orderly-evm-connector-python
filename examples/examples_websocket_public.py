e275cc987c567958ab90ee64f0f7497ea1699433
Castlebin <Castlebin@gmail.com>
2024-05-27 20:20:13 +0800

    Revert "t"
    This reverts commit 0938996be1dcfe190bfabd13b43be9c1befbbb8a.

--- examples/examples_websocket_public.py
+++ examples/examples_websocket_public.py
@@ -5,7 +5,6 @@ from utils.config import get_account_info
 import time, logging
 from orderly_evm_connector.websocket.websocket_api import WebsocketPublicAPIClient
 from datetime import datetime
-import threading
 
 (
     orderly_key,
@@ -36,54 +35,36 @@ def message_handler(_, message):
     except ValueError:
         return
 
-
 # Public websocket does not need to pass orderly_key and orderly_secret arguments
 
+wss_client = WebsocketPublicAPIClient(
+    orderly_testnet=orderly_testnet,
+    orderly_account_id=orderly_account_id,
+    wss_id=wss_id,
+    on_message=message_handler,
+    on_close=on_close,
+    debug=True,
+)
 
-class AsyncWss(threading.Thread):
-    def __init__(self, wss_client):
-        threading.Thread.__init__(self)
-        self.wss_client = wss_client
+# #Request orderbook data
+wss_client.request_orderbook('orderbook','PERP_BTC_USDC')
+# #orderbook depth 100 push every 1s
+#wss_client.get_orderbook('PERP_NEAR_USDC@orderbook')
+# #orderbookupdate updated orderbook push every 200ms
+wss_client.get_orderbookupdate('PERP_NEAR_USDC@orderbookupdate')
+wss_client.get_trade('PERP_NEAR_USDC@trade')
+#wss_client.get_24h_ticker('PERP_NEAR_USDC@ticker')
+#wss_client.get_24h_tickers()
+wss_client.get_bbo('PERP_NEAR_USDC@bbo')
+wss_client.get_bbos()
+wss_client.get_kline("PERP_NEAR_USDC@kline_1m")
+wss_client.get_index_price('PERP_ETH_USDC@indexprice')
+#wss_client.get_index_prices()
+# wss_client.get_mark_price('PERP_ETH_USDC@markprice')
+# wss_client.get_mark_prices()
+# wss_client.get_open_interest('PERP_ETH_USDC@openinterest')
+# wss_client.get_estimated_funding_rate('PERP_BTC_USDC@estfundingrate')
+# wss_client.get_liquidation_push()
 
-    def run(self):
-        wss_client = self.wss_client
-        # #Request orderbook data
-        #wss_client.request_orderbook('orderbook','PERP_BTC_USDC')
-        # #orderbook depth 100 push every 1s
-        #wss_client.get_orderbook('PERP_NEAR_USDC@orderbook')
-        # #orderbookupdate updated orderbook push every 200ms
-        wss_client.get_orderbookupdate('PERP_BTC_USDC@orderbookupdate')
-        #wss_client.get_trade('PERP_NEAR_USDC@trade')
-        #wss_client.get_24h_ticker('PERP_NEAR_USDC@ticker')
-        #wss_client.get_24h_tickers()
-        wss_client.get_bbo('PERP_BTC_USDC@bbo')
-        #wss_client.get_bbos()
-        #wss_client.get_kline("PERP_NEAR_USDC@kline_1m")
-        #wss_client.get_index_price('PERP_ETH_USDC@indexprice')
-        #wss_client.get_index_prices()
-        # wss_client.get_mark_price('PERP_ETH_USDC@markprice')
-        # wss_client.get_mark_prices()
-        # wss_client.get_open_interest('PERP_ETH_USDC@openinterest')
-        # wss_client.get_estimated_funding_rate('PERP_BTC_USDC@estfundingrate')
-        # wss_client.get_liquidation_push()
-
-        time.sleep(1000)
-        wss_client.stop()
-
-
-for i in range(10):
-    wss_client = WebsocketPublicAPIClient(
-        orderly_testnet=orderly_testnet,
-        orderly_account_id=orderly_account_id,
-        wss_id=wss_id + str(i),
-        on_message=message_handler,
-        on_close=on_close,
-        debug=True,
-    )
-
-    thread = AsyncWss(wss_client)
-    thread.start()
-    thread.join()
-    time.sleep(1)
-    print("Thread finished...exiting")
-    break
+time.sleep(1000)
+wss_client.stop()
